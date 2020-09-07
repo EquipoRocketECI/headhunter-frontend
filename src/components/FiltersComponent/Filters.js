68aa5eb322114d73164132c9fe2874cfe65be63a
Daniel Cifuentes <danixbox1313@gmail.com>
2020-09-07 16:48:40 -0500

    css adjustements to filter component

--- src/components/FiltersComponent/Filters.js
+++ src/components/FiltersComponent/Filters.js
@@ -1,103 +1,119 @@
 import React from 'react';
-import { List, ListItem, Divider, FormGroup, FormControlLabel, Checkbox, RadioGroup, Radio, TextField } from '@material-ui/core';
+import { List, ListItem, Divider, FormGroup, FormControlLabel, Checkbox, RadioGroup, Radio, TextField, InputAdornment } from '@material-ui/core';
 import './css/filters.css';
 
-export class Filters extends React.Component{
-    constructor(props){
+export class Filters extends React.Component {
+    constructor(props) {
         super(props);
-        this.state={
-            rangeRadioValue:"none",
-            selectedAreas:{
-                educacion:false,
-                salud:false,
-                comida:false,
-                viaje:false
+        this.state = {
+            rangeRadioValue: "none",
+            selectedAreas: {
+                educacion: false,
+                salud: false,
+                comida: false,
+                viaje: false
             },
-            highRange:-1,
-            lowRange:-1
+            highRange: -1,
+            lowRange: -1
         }
-        this.handleInvestmentChange=this.handleInvestmentChange.bind(this);
-        this.handleAreaChange=this.handleAreaChange.bind(this);
+        this.handleInvestmentChange = this.handleInvestmentChange.bind(this);
+        this.handleAreaChange = this.handleAreaChange.bind(this);
     }
 
-    handleInvestmentChange(e){
-        this.setState({rangeRadioValue:e.target.value,lowRange:-1,highRange:-1});
-        
+    handleInvestmentChange(e) {
+        this.setState({ rangeRadioValue: e.target.value, lowRange: -1, highRange: -1 });
+
     }
 
-    handleAreaChange(e){
-        const areas =this.state.selectedAreas;
-        this.setState({selectedAreas:{...areas,[e.target.name]:e.target.checked}});
+    handleAreaChange(e) {
+        const areas = this.state.selectedAreas;
+        this.setState({ selectedAreas: { ...areas, [e.target.name]: e.target.checked } });
     }
 
-    render(){
-        return(
+    render() {
+        return (
             <List onChange={this.props.getSelectedFilters(this.state)} className="filters">
                 <ListItem className="investmentFilter">
-                    <FormGroup>
-                        <TextField id="lowRange" onChange={(e)=>{
-                            this.setState({
-                                rangeRadioValue:"none",
-                                lowRange: parseInt(e.target.value)
-                            })
-                        }} label="Low bound" variant="outlined"/>
-                        <TextField id="highRange" onChange={
-                            (e)=>{
+                    <FormGroup className="customRangeForm">
+                        <TextField className="customRangeInput"
+                            id="lowRange"
+                            size="small"
+                            InputProps={{
+                                startAdornment: <InputAdornment>$</InputAdornment>
+                            }}
+                            onChange={(e) => {
                                 this.setState({
-                                    rangeRadioValue:"none",
-                                    highRange: parseInt(e.target.value)
+                                    rangeRadioValue: "none",
+                                    lowRange: parseInt(e.target.value)
                                 })
-                            }
-                        } label="High bound" variant="outlined"/>
+                            }} margin="Low bound" variant="outlined" />
+                        <TextField className="customRangeInput"
+                            id="highRange"
+                            size="small"
+                            InputProps={{
+                                startAdornment: <InputAdornment>$</InputAdornment>
+                            }}
+                            onChange={
+                                (e) => {
+                                    this.setState({
+                                        rangeRadioValue: "none",
+                                        highRange: parseInt(e.target.value)
+                                    })
+                                }
+                            } defaultValue="100.00" label="High bound" variant="outlined" />
                     </FormGroup>
                     <RadioGroup value={this.state.rangeRadioValue} onChange={this.handleInvestmentChange}>
-                        <FormControlLabel value="low" control={<Radio/>} label="LOW RANGE"/>
-                        <FormControlLabel value="mid" control={<Radio/>} label="MID RANGE"/>
-                        <FormControlLabel value="high" control={<Radio/>} label="HIGH RANGE"/>
+                        <FormControlLabel value="low" control={<Radio color="black" />} label="LOW RANGE" />
+                        <FormControlLabel value="mid" control={<Radio color="black" />} label="MID RANGE" />
+                        <FormControlLabel value="high" control={<Radio color="black" />} label="HIGH RANGE" />
                     </RadioGroup>
                 </ListItem>
-                <Divider/>
+                <Divider />
                 <ListItem className="areaFilter">
-                    <FormGroup>
-                        <FormControlLabel 
+                    <FormGroup className="areaFilterForm">
+                        <FormControlLabel
                             control={
                                 <Checkbox
                                     checked={this.state.selectedAreas.educacion}
                                     onChange={this.handleAreaChange}
                                     name="educacion"
+                                    color="black"
                                 />
                             }
-                            label="Educación"    
+                            label="Educación"
                         />
-                        <FormControlLabel 
+                        <FormControlLabel
                             control={
                                 <Checkbox
-                                checked={this.state.selectedAreas.salud}
-                                onChange={this.handleAreaChange}
+                                    checked={this.state.selectedAreas.salud}
+                                    onChange={this.handleAreaChange}
                                     name="salud"
+                                    color="black"
                                 />
                             }
-                            label="Salud"    
+                            label="Salud"
                         />
-                        <FormControlLabel 
+                        <FormControlLabel
                             control={
                                 <Checkbox
-                                checked={this.state.selectedAreas.comida}
-                                onChange={this.handleAreaChange}
+                                    checked={this.state.selectedAreas.comida}
+                                    onChange={this.handleAreaChange}
                                     name="comida"
+                                    color="black"
                                 />
                             }
-                            label="Comida"    
+                            label="Comida"
                         />
-                        <FormControlLabel 
+                        <FormControlLabel
                             control={
                                 <Checkbox
-                                checked={this.state.selectedAreas.viaje}
-                                onChange={this.handleAreaChange}
+                                    checked={this.state.selectedAreas.viaje}
+                                    onChange={this.handleAreaChange}
                                     name="viaje"
+                                    color="black"
                                 />
                             }
-                            label="Viaje"    
+                            label="Viaje"
                         />
                     </FormGroup>
                 </ListItem>
